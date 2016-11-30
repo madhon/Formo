950a43d8abecdf16bcf7383c6d1f0f581b855328
Jonathon Douglas <jdouglas@acquisinsurance.com>
2016-11-30 15:11:45 +0000

    Use Paket and F# Make

---
+++ build.fsx
@@ -0,0 +1,40 @@
+// include Fake lib
+#r @"packages\FAKE\tools\FakeLib.dll"
+
+open Fake
+open Fake.AssemblyInfoFile
+open Fake.MSBuildHelper
+open Fake.Testing.XUnit2
+
+let buildDir  = @".\bin\"
+let packagesDir = @".\packages"
+
+let solutionFile  = "Formo.sln"
+
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir]
+)
+
+Target "Compile" (fun _ ->
+    !! @"src\**\*.csproj"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
+
+Target "XUnitTest" (fun _ ->
+    !! (buildDir + @"\Formo.Tests.dll")
+      |> xUnit2 (fun p ->
+                 {p with ShadowCopy = true; HtmlOutputPath = Some (buildDir @@ "xunit.html") })
+)
+
+Target "All" DoNothing
+
+"All"
+  ==> "XUnitTest"
+
+"Clean"
+  ==> "Compile"
+  ==> "XUnitTest"
+
+// start build
+RunTargetOrDefault "XUnitTest"
